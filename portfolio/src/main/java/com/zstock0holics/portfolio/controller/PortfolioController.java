de5b8c0ff0206d7b38aa041f7228b94275c74dcc
Vikas Bacchewar <vikas.bacchewar@zebra.com>
2022-10-12 13:13:05 +0530

    Cross Origin

--- portfolio/src/main/java/com/zstock0holics/portfolio/controller/PortfolioController.java
+++ portfolio/src/main/java/com/zstock0holics/portfolio/controller/PortfolioController.java
@@ -3,6 +3,7 @@ package com.zstock0holics.portfolio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.zstock0holics.portfolio.entity.Stock;
 import com.zstock0holics.portfolio.entity.UserStock;
 import com.zstock0holics.portfolio.service.PortfolioService;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController(value="zstocks")
 public class PortfolioController {
 
--- stock/src/main/java/com/zstock0holics/stock/controller/StockController.java
+++ stock/src/main/java/com/zstock0holics/stock/controller/StockController.java
@@ -3,13 +3,14 @@ package com.zstock0holics.stock.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zstock0holics.stock.entity.Stock;
 import com.zstock0holics.stock.service.StockService;
 
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class StockController {
 	
