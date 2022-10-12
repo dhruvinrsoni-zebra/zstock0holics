62bc3e467ab7df4fadf540df586472c9a08ac832
Dhruvin Rupesh Soni <dhruvin.soni@zebra.com>
2022-10-12 13:59:12 +0530

    Enable CrossOrigin Requests
    1. Added the annotation for the same

--- portfolio/src/main/java/com/zstock0holics/portfolio/controller/PortfolioController.java
+++ portfolio/src/main/java/com/zstock0holics/portfolio/controller/PortfolioController.java
@@ -3,13 +3,14 @@ package com.zstock0holics.portfolio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.zstock0holics.portfolio.entity.Stock;
 import com.zstock0holics.portfolio.entity.UserStock;
 import com.zstock0holics.portfolio.service.PortfolioService;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController(value="zstocks")
 public class PortfolioController {
 
