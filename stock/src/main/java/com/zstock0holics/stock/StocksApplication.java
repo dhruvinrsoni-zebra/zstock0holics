0b9b62d35cbc1baf0e4b50001b1ae0ded5f31cbe
Dhruvin Rupesh Soni <dhruvin.soni@zebra.com>
2022-10-12 10:41:02 +0530

    Initial Commit
    Added the stock, portfolio projects
    Added the frontend folder

---
+++ stock/src/main/java/com/zstock0holics/stock/StocksApplication.java
@@ -0,0 +1,13 @@
+package com.zstock0holics.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksApplication.class, args);
+	}
+
+}
